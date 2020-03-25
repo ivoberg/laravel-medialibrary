1847d3dbcfe930a2956761c09976fccf808fc1ef
ivoberg <ivo@bergx2.de>
2020-03-25 22:28:14 +0000

    attachableModuleTypes

--- src/MediaCollections/Models/Media.php
+++ src/MediaCollections/Models/Media.php
@@ -35,6 +35,12 @@ class Media extends Model implements Responsable, Htmlable
 
     protected $table = 'media';
 
+    private $attachableModuleTypes = [
+        'CasinoGames' => 'casinoGames',
+        'StaticPages' => 'staticPages',
+        'Teasers' => 'teasers',
+        'Carousels' => 'carousels'];
+
     const TYPE_OTHER = 'other';
 
     protected $guarded = [];
@@ -45,16 +51,26 @@ class Media extends Model implements Responsable, Htmlable
         'generated_conversions' => 'array',
         'responsive_images' => 'array',
     ];
-
+    
     public function newCollection(array $models = [])
     {
         return new MediaCollection($models);
     }
-
+    public function modules(): Array
+    {
+        $arr = [];
+        foreach ($this->attachableModuleTypes as $type) {
+            $arr[$type] = $this->morphedByMany('App\Models\\'.$type, 'model', 'model_has_media', 'media_id', 'model_id')->withPivot('model_story_id')->get();
+        }
+        return $arr;
+    }
+    public function module($type): MorphToMany
     {
         return $this->morphedByMany('App\Models\\'.$type, 'model', 'model_has_media', 'media_id', 'model_id')->withPivot('model_story_id');
     }
-
+    /*
+     * Get the full url to a original media file.
+    */
     public function getFullUrl(string $conversionName = ''): string
     {
         return url($this->getUrl($conversionName));
