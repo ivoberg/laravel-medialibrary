050d842b9ecb55e52063f76e96a1fc0f259131b2
ivoberg <ivoberg@users.noreply.github.com>
2021-03-27 15:54:54 +0000

    Fix styling

--- src/MediaCollections/Models/Media.php
+++ src/MediaCollections/Models/Media.php
@@ -7,7 +7,6 @@ use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
